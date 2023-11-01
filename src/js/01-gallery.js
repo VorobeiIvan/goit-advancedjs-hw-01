64d84aac57670a8074a36cd911422a007277b9b4
VorobeiIvan <0675361905vi@gmail.com>
2023-11-01 20:23:26 +0200

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,59 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryImages = document.querySelectorAll('.gallery__image');
+
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    )
+    .join('');
+}
+
+gallery.innerHTML = createMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captions: true,
+});
+
+gallery.addEventListener('click', openOriginalIMG);
+let modalOpen = false;
+
+function openOriginalIMG(e) {
+  e.preventDefault();
+
+  const imgOriginal = e.target.closest('.gallery__link');
+
+  lightbox.open({
+    items: [
+      {
+        url: imgOriginal.getAttribute('href'),
+        title: 'Image Title',
+      },
+    ],
+  });
+
+  modalOpen = true;
+  document.addEventListener('keydown', closeModal);
+}
+
+function closeModal(event) {
+  if (event.key === 'Escape' && modalOpen) {
+    lightbox.close();
+    modalOpen = false;
+    document.removeEventListener('keydown', closeModal);
+  }
+}
