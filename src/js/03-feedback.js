2ee00dbc264de523b8edf155107ef98d03e72539
VorobeiIvan <0675361905vi@gmail.com>
2023-11-01 22:32:44 +0200

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageTextarea = feedbackForm.querySelector('textarea[name="message"]');
+
+const feedbackStorageKey = 'feedback-form-state';
+
+function saveFormState() {
+  const formState = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(feedbackStorageKey, JSON.stringify(formState));
+}
+
+function loadFormState() {
+  const storedState = localStorage.getItem(feedbackStorageKey);
+  if (storedState) {
+    const formState = JSON.parse(storedState);
+    emailInput.value = formState.email;
+    messageTextarea.value = formState.message;
+  }
+}
+
+emailInput.addEventListener('input', throttle(saveFormState, 500));
+messageTextarea.addEventListener('input', throttle(saveFormState, 500));
+
+loadFormState();
+
+feedbackForm.addEventListener('submit', e => {
+  e.preventDefault();
+  localStorage.removeItem(feedbackStorageKey);
+  const formState = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  console.log(formState);
+});
